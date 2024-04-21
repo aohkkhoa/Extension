634de5b3d5ecd9a0c2eb732142658cd92628ca1a
KhoaND <ndk1031999@gmail.com>
2024-04-22 06:28:10 +0700

    fix : get size va color va access tmail

--- content.js
+++ content.js
@@ -9,6 +9,33 @@ function getProductInfoAndRedirect() {
         id: document.querySelector('#aliww-click-trigger').getAttribute('data-item')
     };
     console.log(productInfo);
+
+    // Lấy tất cả các phần tử có class là 'skuCate'
+    const skuCateElements = document.querySelectorAll('.skuCate');
+    skuCateElements.forEach(skuCateElement => {
+        const skuCateText = skuCateElement.querySelector('.skuCateText').textContent.trim();
+        // Kiểm tra nếu chứa từ khóa '颜色'
+        if (skuCateText.includes('颜色') || skuCateText.includes('màu sắc')) {
+            const colors = []; 
+            const colorItem = skuCateElement.querySelectorAll('.skuItem .skuValueName');
+            colorItem.forEach(colorItem => {
+                colors.push(colorItem.textContent.trim());
+            });
+            productInfo.color = colors;
+            
+        }
+        // Kiểm tra nếu chứa từ khóa '尺码'
+        if (skuCateText.includes('尺码') || skuCateText.includes('kích cỡ')) {
+            const sizes = [];
+            const sizeItems = skuCateElement.querySelectorAll('.skuItem .skuValueName');
+            sizeItems.forEach(sizeItem => {
+                sizes.push(sizeItem.textContent.trim());
+            });
+            // Thêm thông tin kích cỡ vào productInfo
+            productInfo.sizes = sizes;
+        }
+    });
+
 // Lấy danh sách các mục đã lưu trữ từ chrome.storage
 chrome.storage.local.get({ carts: [] }, function(result) {
         const existingProduct = result.carts.find(item => item.id === productInfo.id);
@@ -16,12 +43,29 @@ chrome.storage.local.get({ carts: [] }, function(result) {
         if (existingProduct) {
             // Thêm productInfo.image vào mảng existingProduct.image
             existingProduct.image.push(productInfo.image);
+            // Khởi tạo mảng colors nếu chưa tồn tại
+            if (!existingProduct.colors) {
+                existingProduct.colors = [];
+            }
+            // Thêm productInfo.color vào mảng existingProduct.colors nếu tồn tại
+            if (productInfo.color) {
+                existingProduct.colors.push(productInfo.color);
+            }
+            // Khởi tạo mảng sizes nếu chưa tồn tại
+            if (!existingProduct.sizes) {
+                existingProduct.sizes = [];
+            }
+            // Thêm productInfo.sizes vào mảng existingProduct.sizes nếu tồn tại
+            if (productInfo.sizes) {
+                existingProduct.sizes.push(...productInfo.sizes);
+            }
+
         } else {
             productInfo.image = [productInfo.image];
             // Thêm sản phẩm mới vào danh sách
             result.carts.push(productInfo);
         }
-
+        console.log(productInfo);
         // Lưu trữ danh sách đã cập nhật vào chrome.storage
         chrome.storage.local.set({ carts: result.carts }, function() {
         });
